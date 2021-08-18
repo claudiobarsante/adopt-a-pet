9c9c53fe14cdeeabbaeab834191869b26a5c09b1
Claudio Barsante <clbmribas@gmail.com>
2021-08-18 20:16:09 -0300

    test: without PetForm

--- src/pages/pet/form.tsx
+++ src/pages/pet/form.tsx
@@ -1,38 +1,43 @@
-import DropDown from '../../components/Dropdown';
-import { GetStaticProps } from 'next';
-import { getBaseInfosService } from './../../api/services/formService';
+// import DropDown from '../../components/Dropdown';
+// import { GetStaticProps } from 'next';
+// import { getBaseInfosService } from './../../api/services/formService';
 
-type Content = {
-  text: string;
-  value: string;
-};
-type Props = {
-  breeds: Content[];
-  colors: Content[];
-  furs: Content[];
-  sizes: Content[];
-  tempers: Content[];
-  species: Content[];
-};
-const PetForm = ({ breeds, colors, furs, sizes, tempers, species }: Props) => {
-  return (
-    <>
-      <h1>Pet Form</h1>
-      <div>
-        <DropDown data={breeds} type="breed" />
-      </div>
-    </>
-  );
+// type Content = {
+//   text: string;
+//   value: string;
+// };
+// type Props = {
+//   breeds: Content[];
+//   colors: Content[];
+//   furs: Content[];
+//   sizes: Content[];
+//   tempers: Content[];
+//   species: Content[];
+// };
+// const PetForm = ({ breeds, colors, furs, sizes, tempers, species }: Props) => {
+//   return (
+//     <>
+//       <h1>Pet Form</h1>
+//       <div>
+//         <DropDown data={breeds} type="breed" />
+//       </div>
+//     </>
+//   );
+// };
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const result = await getBaseInfosService();
+
+//   const info = JSON.parse(result.data);
+//   const { breeds, colors, furs, sizes, tempers, species } = info;
+
+//   return {
+//     props: { breeds, colors, furs, sizes, tempers, species }
+//   };
+// };
+// export default PetForm;
+const PetForm = () => {
+  return <h1>Form</h1>;
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const result = await getBaseInfosService();
-
-  const info = JSON.parse(result.data);
-  const { breeds, colors, furs, sizes, tempers, species } = info;
-
-  return {
-    props: { breeds, colors, furs, sizes, tempers, species }
-  };
-};
 export default PetForm;
